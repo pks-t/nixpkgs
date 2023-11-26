94a9d691a644312e0d313ad13ff082a9e1240142
Patrick Steinhardt <ps@pks.im>
2023-11-26 17:00:26 +0100

    pkgs: add wlroots 0.17

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32653,6 +32653,7 @@ with pkgs;
   inherit (callPackages ../development/libraries/wlroots {})
     wlroots_0_15
     wlroots_0_16
+    wlroots_0_17
     wlroots;
 
   sway-unwrapped = callPackage ../applications/window-managers/sway {
