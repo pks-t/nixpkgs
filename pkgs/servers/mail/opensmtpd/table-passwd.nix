4473c72c492ce727b9cf72a340985174dc874a7b
Patrick Steinhardt <ps@pks.im>
2024-12-16 08:03:19 +0100

    opensmtpd.table-passwd: init at 1.0.2
    The opensmtpd-extra package has been deprecated in favor of standalone
    repositories for each of the filters. Introduce table-passwd, which is
    one of these new standalone filters to read login info from "passwd"
    files.

---
+++ pkgs/servers/mail/opensmtpd/table-passwd.nix
@@ -0,0 +1,44 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoconf,
+  automake,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "opensmtpd-table-passwd";
+  version = "1.0.2";
+
+  src = fetchFromGitHub {
+    owner = "OpenSMTPD";
+    repo = "table-passwd";
+    rev = finalAttrs.version;
+    hash = "sha256-veE7PADO8KAMEnMrDc9V/xbVMqwF3rUoYPmpQSIJw9o=";
+  };
+
+  nativeBuildInputs = [
+    autoconf
+    automake
+  ];
+
+  configureFlags = [
+    "--sysconfdir=/etc"
+    "--localstatedir=/var"
+    "--with-path-socket=/run"
+    "--with-path-pidfile=/run"
+  ];
+
+  preConfigure = ''
+    sh bootstrap
+  '';
+
+  meta = with lib; {
+    homepage = "https://www.opensmtpd.org/";
+    description = "passwd table for the OpenSMTPD mail server";
+    license = licenses.mit;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [
+      pks-gitlab
+    ];
+  };
+})
