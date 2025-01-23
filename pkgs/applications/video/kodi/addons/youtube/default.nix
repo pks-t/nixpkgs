46d01e98aba04444b83f6698509a256ae21a451c
Patrick Steinhardt <ps@pks.im>
2025-01-23 05:47:12 +0100

    kodaPackages.youtube: update to 7.2.0+beta.5

--- pkgs/applications/video/kodi/addons/youtube/default.nix
+++ pkgs/applications/video/kodi/addons/youtube/default.nix
@@ -10,13 +10,13 @@
 buildKodiAddon rec {
   pname = "youtube";
   namespace = "plugin.video.youtube";
-  version = "7.2.0+beta.4";
+  version = "7.2.0+beta.5";
 
   src = fetchFromGitHub {
     owner = "anxdpanic";
     repo = "plugin.video.youtube";
-    rev = "v${version}";
-    hash = "sha256-JWaHZ15dYrNdLkLuMRDe9BQp8ZhhctCIHw6VwONI6gU=";
+    rev = "ee061bd30ff168c5541671845de254ec4ef34fdc";
+    hash = "sha256-0RzMrkycQ6lVlLzbopqCO2UPeduo1aNdQBYF8g7AhXs=";
   };
 
   propagatedBuildInputs = [
